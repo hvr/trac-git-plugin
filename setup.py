d5940e0618a845589c089afd364f0f82b84d7309
Herbert Valerio Riedel <hvr@gnu.org>
2009-03-23 18:00:56 +0000

    GitPlugin: update the ''install_requires'' setting and bump the plugin version to 0.11.0.2

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 setup(
     name='TracGit',
-    install_requires='Trac >=0.11dev, ==0.11b1',
+    install_requires='Trac ==0.11, ==0.11rc2, ==0.11rc1, ==0.11b2, ==0.11b1, >=0.11dev',
     description='GIT version control plugin for Trac 0.11',
     author='Herbert Valerio Riedel',
     author_email='hvr@gnu.org',
     keywords='trac scm plugin git',
     url="http://trac-hacks.org/wiki/GitPlugin",
-    version='0.11.0.1',
+    version='0.11.0.2',
     license="GPL",
     long_description="""
     This Trac 0.11 plugin provides support for the GIT SCM.
