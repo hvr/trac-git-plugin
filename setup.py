44cb43f67939d280ca047f6d2c63bd38c9e39618
Herbert Valerio Riedel <hvr@gnu.org>
2010-10-16 13:11:34 +0200

    bump up version to 0.12.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='TracGit',
       author_email='hvr@gnu.org',
       keywords='trac scm plugin git',
       url="http://trac-hacks.org/wiki/GitPlugin",
-      version='0.12.0.2',
+      version='0.12.0.3',
       license="GPL",
       long_description="""
       This Trac 0.12 plugin provides support for the GIT SCM.
