52986437b322f52ed420df639212c071f12ef163
Herbert Valerio Riedel <hvr@gnu.org>
2010-10-16 11:36:55 +0200

    Clean up `setup.py` and add Trac >= 0.12 dependancy

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@
 
 from setuptools import setup
 
-setup(
-    name='TracGit',
-    install_requires=[],#'Trac ==0.12',
-    description='GIT version control plugin for Trac 0.12',
-    author='Herbert Valerio Riedel',
-    author_email='hvr@gnu.org',
-    keywords='trac scm plugin git',
-    url="http://trac-hacks.org/wiki/GitPlugin",
-    version='0.12.0.2',
-    license="GPL",
-    long_description="""
-    This Trac 0.12 plugin provides support for the GIT SCM.
+setup(name='TracGit',
+      install_requires='Trac >= 0.12',
+      description='GIT version control plugin for Trac 0.12',
+      author='Herbert Valerio Riedel',
+      author_email='hvr@gnu.org',
+      keywords='trac scm plugin git',
+      url="http://trac-hacks.org/wiki/GitPlugin",
+      version='0.12.0.2',
+      license="GPL",
+      long_description="""
+      This Trac 0.12 plugin provides support for the GIT SCM.
 
-    See http://trac-hacks.org/wiki/GitPlugin for more details.
-    """,
-    packages=['tracext', 'tracext.git'],
-    namespace_packages=['tracext'],
-    entry_points = {'trac.plugins': 'git = tracext.git.git_fs'},
-    data_files=['COPYING','README'])
+      See http://trac-hacks.org/wiki/GitPlugin for more details.
+      """,
+      packages=['tracext', 'tracext.git'],
+      namespace_packages=['tracext'],
+      entry_points = {'trac.plugins': 'git = tracext.git.git_fs'},
+      package_data={'': ['COPYING','README']}
+)
